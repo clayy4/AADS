f54ad0fd8c9c1cd954887c32db20eb6e8534c7f6
clayw <clayworks11337@gmail.com>
2025-09-22 19:28:32 +0400

    Create class

---
+++ Lab1_var6_task3/library/grayscale.hpp
@@ -0,0 +1,19 @@
+#ifndef GRAYSCALE_HPP
+#define GRAYSCALE_HPP
+
+
+template <typename T>
+class GrScImage { //GrayScaleImage
+private:
+	int _rows;
+	int _cols;
+	int* _matrix;
+public:
+	//GrScImage() :{}
+	//operator[]
+	//operator+
+	//operator*
+	//operator-
+};
+
+#endif
